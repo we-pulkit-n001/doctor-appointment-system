b6cb6c882d22a3e8fa897d6629301096ae8c6f2d
Pulkit Nagpal <pulkit-n001@webreinvent.com>
2024-10-28 18:12:05 +0530

    Added: Doughnut Chart in the dashboard section to Showcase booked and cancelled Appointments

--- VaahCms/Modules/Assignment/Vue/pages/dashboard/Dashboard.vue
+++ VaahCms/Modules/Assignment/Vue/pages/dashboard/Dashboard.vue
@@ -1,8 +1,8 @@
-<script  setup>
-import {onMounted, ref, watch} from "vue";
+<script setup>
+import { onMounted, ref } from "vue";
 import { vaah } from '../../vaahvue/pinia/vaah';
-import { useAppointmentStore } from '../../stores/store-appointments'
-import {useRoute} from 'vue-router';
+import { useAppointmentStore } from '../../stores/store-appointments';
+import { useRoute } from 'vue-router';
 
 const store = useAppointmentStore();
 const route = useRoute();
@@ -11,6 +11,8 @@ const useVaah = vaah();
 
 const chartData = ref();
 const chartOptions = ref();
+const doughnutChartData = ref();
+const doughnutChartOptions = ref();
 
 const registered_doctors = ref(null);
 const total_appointments = ref(null);
@@ -21,30 +23,39 @@ const total_patients = ref(null);
 onMounted(async () => {
     await store.getDashboardData();
 
-        registered_doctors.value = store.item?.data.registered_doctors ?? 0;
-        total_appointments.value = store.item?.data.total_appointments ?? 0;
-        cancelled_appointments.value = store.item?.data.cancelled_appointments ?? 0;
-        revenue_till_date.value = store.item?.data.revenue_till_date ?? 0;
-        total_patients.value = store.item?.data.total_patients ?? 0;
+    registered_doctors.value = store.item?.data.registered_doctors ?? 0;
+    total_appointments.value = store.item?.data.total_appointments ?? 0;
+    cancelled_appointments.value = store.item?.data.cancelled_appointments ?? 0;
+    revenue_till_date.value = store.item?.data.revenue_till_date ?? 0;
+    total_patients.value = store.item?.data.total_patients ?? 0;
 
     chartData.value = setChartData();
     chartOptions.value = setChartOptions();
 
+    doughnutChartData.value = setDoughnutChartData();
+    doughnutChartOptions.value = setDoughnutChartOptions();
 });
 
 const setChartData = () => {
     return {
-        labels: ['Registered Doctors', 'Overall Patients Registered', 'Appointments Booked', 'Appointments Cancelled'],
+        labels: ['Registered Doctors', 'Overall Patients Registered'],
         datasets: [
             {
                 label: 'Statistics',
-                data: [store.item.data.registered_doctors, store.item.data.total_patients, store.item.data.total_appointments, store.item.data.cancelled_appointments],
-                backgroundColor: ['rgba(50, 50, 50, 0.5)', 'rgba(150, 150, 150, 0.5)', 'rgba(100, 100, 100, 0.2)', 'rgba(50, 50, 50, 0.4)'],
+                data: [
+                    store.item.data.registered_doctors,
+                    store.item.data.total_patients,
+                ],
+                backgroundColor: [
+                    'rgba(50, 50, 50, 0.5)',
+                    'rgba(150, 150, 150, 0.5)',
+                ],
                 borderWidth: 1
             }
         ]
     };
 };
+
 const setChartOptions = () => {
     const documentStyle = getComputedStyle(document.documentElement);
     const textColor = documentStyle.getPropertyValue('--p-text-color');
@@ -52,6 +63,8 @@ const setChartOptions = () => {
     const surfaceBorder = documentStyle.getPropertyValue('--p-content-border-color');
 
     return {
+        responsive: true,
+        maintainAspectRatio: false,
         plugins: {
             legend: {
                 labels: {
@@ -79,7 +92,45 @@ const setChartOptions = () => {
             }
         }
     };
-}
+};
+
+const setDoughnutChartData = () => {
+    return {
+        labels: ['Appointments Booked', 'Appointments Cancelled'],
+        datasets: [
+            {
+                data: [
+                    store.item.data.total_appointments,
+                    store.item.data.cancelled_appointments
+                ],
+                backgroundColor: [
+                    'rgba(100, 100, 100, 0.2)',
+                    'rgba(50, 50, 50, 0.4)'
+                ],
+                hoverBackgroundColor: [
+                    'rgba(100, 100, 100, 0.4)',
+                    'rgba(50, 50, 50, 0.6)'
+                ]
+            }
+        ]
+    };
+};
+
+const setDoughnutChartOptions = () => {
+    const documentStyle = getComputedStyle(document.documentElement);
+    const textColor = documentStyle.getPropertyValue('--text-color');
+
+    return {
+        plugins: {
+            legend: {
+                labels: {
+                    cutout: '60%',
+                    color: textColor
+                }
+            }
+        }
+    };
+};
 
 document.title = 'Assignment';
 </script>
@@ -103,22 +154,28 @@ document.title = 'Assignment';
 
                 <Card class="card">
                     <template #title>
-                        <h5>Overall Patients Registered</h5>
+                        <div class="card-title">
+                            <i class="pi pi-users icon"></i>
+                            <h5>Overall Patients Registered</h5>
+                        </div>
                     </template>
                     <template #content>
                         <h2 v-if="store.item">{{ store.item.data.total_patients }}</h2>
                     </template>
                 </Card>
 
+
+
+
+
                 <Card class="card">
                     <template #title>
                         <h5>Appointments Booked</h5>
                     </template>
                     <template #content>
-                            <h2 v-if="store.item">{{ store.item.data.total_appointments }}</h2>
+                        <h2 v-if="store.item">{{ store.item.data.total_appointments }}</h2>
                     </template>
                 </Card>
-
 
                 <Card class="card">
                     <template #title>
@@ -140,13 +197,19 @@ document.title = 'Assignment';
             </div>
         </div>
 
-        <!-- Adding Chart view -->
-        <div class="chart-card">
-            <Card>
-                <template #content>
+        <!-- Modified Charts Container -->
+        <div class="charts-container">
+            <div class="chart-card bar-chart-card">
+                <div class="bar-chart">
                     <Chart type="bar" :data="chartData" :options="chartOptions" />
-                </template>
-            </Card>
+                </div>
+            </div>
+
+            <div class="chart-card doughnut-chart-card">
+                <div class="doughnut-chart">
+                    <Chart type="doughnut" :data="doughnutChartData" :options="doughnutChartOptions" />
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -199,20 +262,76 @@ h1 {
     margin-right: 10px;
 }
 
-.chart-card {
-    margin-top: 20px;
-    background-color: white;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-    width: 100%;
-    max-width: 800px;
-    margin-left: auto;
-    margin-right: auto;
+/* Modify the heading style for card titles */
+.card h5 {
+    font-weight: normal; /* Change to normal weight for less boldness */
+    margin: 0; /* Remove margin for better alignment */
 }
 
-.chart-card canvas {
+/* Modified Charts Container Styles */
+.charts-container {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 20px;
+    flex-wrap: wrap;
+}
+
+.chart-card {
+    width: 48%;
+    max-width: 100%;
+    background-color: white;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 350px;
+}
+
+.bar-chart,
+.doughnut-chart {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+/* Specific styles for bar chart to ensure it fills the container */
+.bar-chart-card .bar-chart :deep(canvas) {
+    width: 100% !important;
     height: 100% !important;
-    width: auto !important;
+}
+
+.card-title {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start; /* Aligns title to the left */
+    margin-bottom: 10px; /* Add space below title */
+}
+
+.card-title .icon {
+    font-size: 20px; /* Adjust icon size as needed */
+    margin-right: 5px; /* Space between icon and title */
+}
+
+.card-title {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start; /* Aligns title to the left */
+    margin-bottom: 10px; /* Space below title */
+}
+
+.card-title h5 {
+    font-weight: bold; /* Ensure it is bold */
+    font-size: 1.25rem; /* Adjust the size to match other card titles */
+    margin: 0; /* Remove default margin for alignment */
+}
+
+.card-title .icon {
+    font-size: 20px; /* Adjust icon size as needed */
+    margin-right: 5px; /* Space between icon and title */
 }
 
 
 </style>
+
